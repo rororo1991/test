cf4bade3676263203f28a64f254e64f800e4000e
rororo1991 <30346488+rororo1991@users.noreply.github.com>
2017-08-22 18:06:20 +0300

    create github pages

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -6,7 +6,7 @@
         var e = document.createElement("section");
         e.id = "top-1";
         e.className = "engine";
-        e.innerHTML = '<a href="https://mobirise.info">Mobirise</a> Mobirise v4.0.15';
+        e.innerHTML = '<a href="https://mobirise.info">Mobirise</a> Mobirise v4.1.7';
         document.body.insertBefore(e, document.body.childNodes[0]);
     }
 }();
